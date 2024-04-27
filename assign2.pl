d408dd481aff054fd338b4d43e40ff1065f881f9
saraalbagory <saraanabil123@gmail.com>
2024-04-27 08:59:31 -0700

    Al_findCycle

---
+++ assign2.pl
@@ -0,0 +1,26 @@
+:- dynamic (state/3).
+/*state(color,col,row) */
+
+%predicate to create the board:- J>=Col
+reateState(_, _, _, [], []).
+createState(Row,Col,Row,Colors,NewList).
+createState(J,Col,Row,[Color|Colors],NewList):- J<Col, assertz(state(Color, J, Row)),Jt is J + 1,createState(Jt,Col,Row,Colors,Colors).
+
+
+createRow(_,_,_,[]). 
+createRow(I,_,Row,Colors) :-I>=Row.
+createRow(I,Col,Row,Colors):-I<Row.
+    createState(0,Col,I,Colors,NewList),
+    NewI is I+1,
+    createRow(NewI,Col,Row,NewList)
+.
+
+/*create a N^M board with the Board list which is a list of colors b: blue, r for red , y for yellow*/
+
+ findCycle(N,M,Board):-createRow(0,N,M,Board).
+%findCycle(4,4,[y,y,y,r,b,y,b,y,b,b,b,y,b,b,b,y]).
+
+% collectStates(States)
+% Returns a list of all states that have been created
+collectStates(States) :-
+    findall(state(Color, Col, Row), state(Color, Col, Row), States).
