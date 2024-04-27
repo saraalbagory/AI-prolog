0a322f5821be79241f2ad758daf3969512b45f13
saraalbagory <saraanabil123@gmail.com>
2024-04-27 12:22:58 -0700

    secound commit

--- assign2.pl
+++ assign2.pl
@@ -1,23 +1,23 @@
 :- dynamic (state/3).
 /*state(color,col,row) */
 
-%predicate to create the board:- J>=Col
-reateState(_, _, _, [], []).
-createState(Row,Col,Row,Colors,NewList).
-createState(J,Col,Row,[Color|Colors],NewList):- J<Col, assertz(state(Color, J, Row)),Jt is J + 1,createState(Jt,Col,Row,Colors,Colors).
+%predicate to create the board:- J>=ColJ>=Col
+%createState(_, _, _, [], []).
+createState(Col,Col,Row,Colors,Colors).
+createState(J,Col,Row,[Color|Colors],NewList):- J<Col, assertz(state(Color, J, Row)),Jt is J + 1,!,createState(Jt,Col,Row,Colors,Colors).
 
 
-createRow(_,_,_,[]). 
+%createRow(_,_,_,[]). 
 createRow(I,_,Row,Colors) :-I>=Row.
-createRow(I,Col,Row,Colors):-I<Row.
+createRow(I,Col,Row,Colors):-I<Row,
+   % J is 0,
     createState(0,Col,I,Colors,NewList),
     NewI is I+1,
-    createRow(NewI,Col,Row,NewList)
-.
+    createRow(NewI,Col,Row,Colors).
 
 /*create a N^M board with the Board list which is a list of colors b: blue, r for red , y for yellow*/
 
- findCycle(N,M,Board):-createRow(0,N,M,Board).
+findCycle(N,M,Board):-createRow(0,N,M,Board).
 %findCycle(4,4,[y,y,y,r,b,y,b,y,b,b,b,y,b,b,b,y]).
 
 % collectStates(States)
